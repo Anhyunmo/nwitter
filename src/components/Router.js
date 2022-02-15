d9d3950077f6990b4d338c68db1c66fab3e49e3e
A <dksah79@naver.com>
2022-02-15 19:41:29 +0900

    test commit

--- src/components/Router.js
+++ src/components/Router.js
@@ -12,7 +12,7 @@ const AppRouter = () => {
                 {isLoggedIn ? (
                 <> 
                 <Route exact path="/">
-                    <Home />
+                    <Home />1
                 </Route> 
                 </>
             ) : (
