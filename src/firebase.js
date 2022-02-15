e081f792b14c1781e4bde1d47360e3a0c304d8b9
dksah79 <dksah79@naver.com>
2022-02-15 16:08:32 +0900

    commit

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBeuPnIRe7-jzyJ-E58FFbW_YPAETu2CEo",
+    authDomain: "nwitter-9f221.firebaseapp.com",
+    projectId: "nwitter-9f221",
+    storageBucket: "nwitter-9f221.appspot.com",
+    messagingSenderId: "763071657067",
+    appId: "1:763071657067:web:a0ae975e84e33002246d2e"
+  };
+
+export default initializeApp(firebaseConfig);
